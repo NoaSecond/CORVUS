2566e204de5f9d91df00fef0cafa0750245923f8
Noa Second <contact@noasecond.com>
2024-05-04 01:18:29 +0200

    :sparkles: Add tabs and device info display

--- GUI.py
+++ GUI.py
@@ -1,22 +1,78 @@
 import sys
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
-from InputHandler import get_device_list
+from InputHandler import get_device_list, get_device_info
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.combo_box = QtWidgets.QComboBox() 
-        self.populate_combo_box()
+        self.window = QtWidgets.QTabWidget()
 
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.combo_box) 
+        # Main Tab
+        self.tab_main = QtWidgets.QWidget()
+        self.window.addTab(self.tab_main, "Main")
+
+        # Devices Tab
+        self.tab_devices = QtWidgets.QWidget()
+        self.window.addTab(self.tab_devices, "Devices")        
+
+        self.combo_box = QtWidgets.QComboBox(self.tab_main)
+        self.populate_combo_box()
+        self.select_button = QtWidgets.QPushButton("Select", self.tab_main)
+        self.select_button.clicked.connect(self.selected_device)
+        self.layout_Devices = QtWidgets.QHBoxLayout(self.tab_main)
+        self.layout_Devices.addWidget(self.combo_box)
+        self.layout_Devices.addWidget(self.select_button)
+        self.tab_devices.setLayout(self.layout_Devices)
+
+        # Widget to display selected device info
+        self.device_info_label = QtWidgets.QLabel(self.tab_devices)
+        self.device_info_label.setText("Selected Device Info:")
+        self.device_info_text = QtWidgets.QTextEdit(self.tab_devices)
+        self.device_info_text.setReadOnly(True)
+        self.layout_device_info = QtWidgets.QVBoxLayout()
+        self.layout_device_info.addWidget(self.device_info_label)
+        self.layout_device_info.addWidget(self.device_info_text)
+        self.layout_Devices.addLayout(self.layout_device_info)
+
+        # Debug Tab
+        self.tab_debug = QtWidgets.QWidget()
+        self.window.addTab(self.tab_debug, "Debug")
+
+        # Terminal for debug info
+        self.debug_terminal = QtWidgets.QTextEdit(self.tab_debug)
+        self.debug_terminal.setReadOnly(True)
+        self.layout_debug_terminal = QtWidgets.QVBoxLayout()
+        self.layout_debug_terminal.addWidget(self.debug_terminal)
+        self.tab_debug.setLayout(self.layout_debug_terminal)
+
+        # Top text before tabs
+        self.selected_device_label = QtWidgets.QLabel(self)
+        self.selected_device_label.setText("Selected device: None")
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.addWidget(self.selected_device_label)
+        layout.addWidget(self.window)
 
     def populate_combo_box(self):
         device_list = get_device_list()
         for device in device_list:
-            self.combo_box.addItem(device['product_name'])
+            self.combo_box.addItem(get_device_info(device).get('product_name'))
+
+    def selected_device(self):
+        selected_index = self.combo_box.currentIndex()
+        device_info = get_device_info(get_device_list()[selected_index])
+        device_info_text = ""
+        
+        for key, value in device_info.items():
+            device_info_text += f"{key}: {value}\n"          
+        
+        self.device_info_text.setPlainText(device_info_text)
+        self.selected_device_label.setText("Selected device: "+device_info.get('product_name'))
+        self.debug(f"Selected device: {device_info.get('product_name')}")        
+        
+    def debug(self, string):
+        self.debug_terminal.clear()
+        self.debug_terminal.append(string)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
--- InputHandler.py
+++ InputHandler.py
@@ -2,34 +2,21 @@ import pywinusb.hid as hid
 from time import sleep
 from msvcrt import kbhit
 
+def get_device_info(device):
+    device_info = {
+        'parent_instance_id': device.parent_instance_id,
+        'serial_number': device.serial_number,
+        'vendor_id': device.vendor_id,
+        'product_id': device.product_id,
+        'version_number': device.version_number,
+        'product_name': device.product_name
+    }
+    return device_info  
+
 def get_device_list():
     devices = hid.HidDeviceFilter().get_devices()
     device_list = []
-    for i, dev in enumerate(devices):
-        device_info = {
-            'index': i,
-            'parent_instance_id': dev.parent_instance_id,
-            'serial_number': dev.serial_number,
-            'vendor_id': dev.vendor_id,
-            'product_id': dev.product_id,
-            'version_number': dev.version_number,
-            'product_name': dev.product_name
-        }
-        device_list.append(device_info)
+    for device in devices:
+        if get_device_info(device).get('product_name')[0] != '@':
+            device_list.append(device)            
     return device_list
-
-def sample_handler(data):
-    print("Raw data: {0:}".format(data))
-
-def display_input_values(device):
-    try:
-        device.open()
-        # set custom raw data handler
-        device.set_raw_data_handler(sample_handler)
-
-        print("\nWaiting for data...\nPress any (system keyboard) key to stop...")
-        while not kbhit() and device.is_plugged():
-            # just keep the device opened to receive events
-            sleep(0.5)
-    finally:
-        device.close()
