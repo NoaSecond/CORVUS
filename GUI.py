d5575058787dea1bf8b12deabe8b7b3f4251d6e4
Noa Second <contact@noasecond.com>
2024-05-04 16:31:36 +0200

    :art: Reformat Structure

--- GUI.py
+++ GUI.py
@@ -1,8 +1,14 @@
 import sys
 from PySide6 import QtCore, QtWidgets, QtGui
-from InputHandler import get_device_list, get_device_info
+from DeviceHandler import *
+
+
+from time import sleep
+from msvcrt import kbhit
 
 class MyWidget(QtWidgets.QWidget):
+    device = None
+    
     def __init__(self):
         super().__init__()
 
@@ -19,21 +25,11 @@ class MyWidget(QtWidgets.QWidget):
         self.combo_box = QtWidgets.QComboBox(self.tab_main)
         self.populate_combo_box()
         self.select_button = QtWidgets.QPushButton("Select", self.tab_main)
-        self.select_button.clicked.connect(self.selected_device)
+        self.select_button.clicked.connect(self.select_device)
         self.layout_Devices = QtWidgets.QHBoxLayout(self.tab_main)
         self.layout_Devices.addWidget(self.combo_box)
         self.layout_Devices.addWidget(self.select_button)
         self.tab_devices.setLayout(self.layout_Devices)
-
-        # Widget to display selected device info
-        self.device_info_label = QtWidgets.QLabel(self.tab_devices)
-        self.device_info_label.setText("Selected Device Info:")
-        self.device_info_text = QtWidgets.QTextEdit(self.tab_devices)
-        self.device_info_text.setReadOnly(True)
-        self.layout_device_info = QtWidgets.QVBoxLayout()
-        self.layout_device_info.addWidget(self.device_info_label)
-        self.layout_device_info.addWidget(self.device_info_text)
-        self.layout_Devices.addLayout(self.layout_device_info)
 
         # Debug Tab
         self.tab_debug = QtWidgets.QWidget()
@@ -56,23 +52,25 @@ class MyWidget(QtWidgets.QWidget):
     def populate_combo_box(self):
         device_list = get_device_list()
         for device in device_list:
-            self.combo_box.addItem(get_device_info(device).get('product_name'))
+            self.combo_box.addItem(get_device_info(device).get('vendor_name')+ " " + get_device_info(device).get('product_name'))
 
-    def selected_device(self):
+    def select_device(self):
         selected_index = self.combo_box.currentIndex()
-        device_info = get_device_info(get_device_list()[selected_index])
-        device_info_text = ""
-        
+        self.device = get_device_list()[selected_index]
+        device_info = get_device_info(self.device)
+        device_info_text = ""        
         for key, value in device_info.items():
-            device_info_text += f"{key}: {value}\n"          
+            device_info_text += f"{key}: {value}\n"
+        self.debug(f"Selected device info:\n{device_info_text}")        
+        self.selected_device_label.setText("Selected device: "+device_info.get('product_name'))   
         
-        self.device_info_text.setPlainText(device_info_text)
-        self.selected_device_label.setText("Selected device: "+device_info.get('product_name'))
-        self.debug(f"Selected device: {device_info.get('product_name')}")        
+        # Get device inputs/outputs
+        count_elements_in_reports(self.device)
+        
         
     def debug(self, string):
-        self.debug_terminal.clear()
         self.debug_terminal.append(string)
+            
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
