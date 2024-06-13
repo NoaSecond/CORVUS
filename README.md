42150e3dbf553ddc6178889f2082fca4c9c42325
Noa Second <contact@noasecond.com>
2024-06-13 21:39:34 +0200

    :sparkles: Update code with Qt Framework

--- README.md
+++
@@ -1,6 +0,0 @@
-# CORVUS
-<a href="https://skillicons.dev"><img src="https://skillicons.dev/icons?i=git,py,qt,arduino"/></a>
-
-CORVUS est une initiative open source qui vise à développer un logiciel de pilotage en Python.
-
-Son objectif est de contrôler un drone à partir d'une plateforme Arduino.
---
+++ qfi/qfi_defs.h
@@ -0,0 +1,49 @@
+/****************************************************************************//*
+ * Copyright (C) 2023 Marek M. Cel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ ******************************************************************************/
+#ifndef QFI_DEFS_H
+#define QFI_DEFS_H
+
+////////////////////////////////////////////////////////////////////////////////
+
+#if defined(_MSC_VER)
+#   if defined(QFI_DLL_EXPORTS)
+#       define QFI_DLL_SPEC __declspec(dllexport)
+#   elif defined(QFI_DLL_IMPORTS)
+#       define QFI_DLL_SPEC __declspec(dllimport)
+#   else
+#       define QFI_DLL_SPEC
+#   endif
+#else
+#    define QFI_DLL_SPEC
+#endif
+
+#if defined(__cplusplus)
+#   define QFIAPI QFI_DLL_SPEC
+#endif
+
+#if !defined(QFIAPI)
+#   define QFIAPI
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+
+#endif // QFI_DEFS_H
